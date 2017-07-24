c917642c74f2dba1ebcd15f130907080b3be1987
Chen He <chen.he2@mail.mcgill.ca>
2017-07-24 12:58:55 -0400

    save

---
+++ Source/Purple_Leaf/CommonHeader.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CommonHeader.h"
+
+CommonHeader::CommonHeader()
+{
+}
+
+CommonHeader::~CommonHeader()
+{
+}
---
+++ Source/Purple_Leaf/CommonHeader.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PURPLE_LEAF_API CommonHeader
+{
+public:
+	CommonHeader();
+	~CommonHeader();
+};
